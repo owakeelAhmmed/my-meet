3ead0684e970903a957e2be33e14dc6ab1d4c43c
owakeelAhmmed <owakeel.ahmmed@gmail.com>
2024-07-05 10:53:04 +0600

    first commit

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import React, { ReactNode } from 'react'
+
+const HomeLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <main className='relative'>
+        <Navbar/>
+        <div className='flex'>
+            <Sidebar/>
+
+            <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-6 max-md:pb-14 sm:pb-14'>
+
+                <div className='w-full'>
+                    {children}
+                </div>
+
+            </section>
+        </div>
+        </main>
+  )
+}
+
+export default HomeLayout
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        labe:'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg'
+      },
+      {
+        labe:'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/icons/upcoming.svg'
+      },
+      {
+        labe:'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+      },
+      {
+        labe:'Recordings',
+        route: '/recordings',
+        imgUrl: '/icons/Video.svg'
+      }, 
+      {
+        labe:'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/icons/add-personal.svg'
+      }
+]
